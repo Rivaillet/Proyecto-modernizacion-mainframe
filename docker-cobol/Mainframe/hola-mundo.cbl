332e748ff9f9d9609692b43d788a612a888ddc69
Juan Barbero <juanbsnfs@gmail.com>
2026-06-15 19:16:29 +0200

    Primer commit: Estructura de carpetas, docker db2 con docker compose y hola mundo cobol

---
+++ docker-cobol/Mainframe/hola-mundo.cbl
@@ -0,0 +1,7 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HOLAMUNDO.
+
+       PROCEDURE DIVISION.
+           DISPLAY '¡Hola Mundo desde GnuCOBOL en Docker!'.
+           STOP RUN.
+           
